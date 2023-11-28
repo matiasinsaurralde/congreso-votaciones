250fc757fcc578dfe523ea14ad2430968b5d1068
Matias Insaurralde <matias@insaurral.de>
2023-11-28 07:09:27 +0200

    Add types package

---
+++ pkg/types/types.go
@@ -0,0 +1,12 @@
+package types
+
+// DocumentType is a type alias for the document type:
+type DocumentType string
+
+// Some hardcoded document type constants:
+const (
+	DocumentTypeA       DocumentType = "a"
+	DocumentTypeB       DocumentType = "b"
+	DocumentTypeC       DocumentType = "c"
+	UnknownDocumentType DocumentType = "unknown"
+)
